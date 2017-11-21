c2e11d9f5769515949a9ff24b7201a84a786601b
Paul Marshall <paulama@ftml.net>
2017-11-21 10:44:18 +0000

    Reduce some of the code clunkiness

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,18 @@ extern crate time;
 use piston_window::*;
 use rand::Rng;
 
+const COURT_HEIGHT: f64 = 700.0;
+const COURT_WIDTH: f64 = 900.0;
 const BAT_WIDTH: f64 = 130.0;
-const BALL_RADIUS: f64 = 10.0;
 const BAT_THICKNESS: f64 = 19.0;
-const COURT_HEIGHT: u32 = 700;
-const COURT_WIDTH: u32 = 900;
+const BAT_Y: f64 = COURT_HEIGHT - 50.0;
 const Y_VEL: f64 = 300.0;
 const X_VEL_SHALLOW: f64 = Y_VEL * 1.5;
 const X_VEL_NORMAL: f64 = Y_VEL;
 const X_VEL_STEEP: f64 = Y_VEL * 0.5;
 const MAX_BALLS: usize = 5;
 const NEW_BALL_INTERVAL: u32 = 5;
-
-const BAT_Y: f64 = COURT_HEIGHT as f64 - 50.0;
+const BALL_RADIUS: f64 = 10.0;
 const BALL_RADIUS_SQ: f64 = BALL_RADIUS * BALL_RADIUS;
 
 #[derive(Copy, Clone, Hash, PartialEq, Eq)]
@@ -69,100 +68,102 @@ impl Score {
             &self.points.to_string(),
             glyphs,
             &c.draw_state,
-            c.transform.trans(((COURT_WIDTH / 2) - 50) as f64, (COURT_HEIGHT / 2) as f64),
+            c.transform.trans((COURT_WIDTH / 2.0) - 50.0, COURT_HEIGHT / 2.0),
             g
             ).unwrap();
     }
 }
 
-fn square(n: f64) -> f64 {
-    n * n
+struct AxisMotion {
+    pos: f64,
+    old_pos: f64,
+    vel: f64,
+}
+
+impl AxisMotion {
+    fn new() -> AxisMotion {
+        AxisMotion{pos: 0.0, old_pos: 0.0, vel: 0.0}
+    }
+}
+
+struct Edge {
+    pos: f64,
+    start: f64,
+    end: f64,
+}
+
+impl Edge {
+    fn infinite(pos: f64) -> Edge {
+        // approximations of -ve and +ve infinity ;-)
+        Edge{pos, start: -10000.0, end: 10000.0}
+    }
+
+    fn from_width(pos:f64, start: f64, width: f64) -> Edge {
+        Edge{pos, start, end: start + width}
+    }
 }
 
 struct Ball {
-    x: f64,
-    y: f64,
-    old_x: f64,
-    old_y: f64,
-    x_vel: f64,
-    y_vel: f64,
+    x: AxisMotion,
+    y: AxisMotion,
     colour: [f32; 4],
     in_play: bool,
 }
 
 impl Ball {
     fn new(colour: [f32; 4]) -> Ball {
-        Ball{x: 0.0, y: 0.0, old_x: 0.0, old_y: 0.0, x_vel: 0.0, y_vel: 0.0,
-            colour, in_play: false}
+        Ball{x: AxisMotion::new(), y: AxisMotion::new(), colour, in_play: false}
     }
 
-    fn strike(coord: f64, orth_coord: f64,
-              old_coord: f64, old_orth_coord: f64,
-              strike_edge: f64, edge_start: f64, edge_end:f64) -> Option<f64> {
-        let adj_strike_edge = if coord > old_coord {strike_edge - BALL_RADIUS}
-                              else {strike_edge + BALL_RADIUS};
-        if (coord > old_coord && coord >= adj_strike_edge) ||
-           (coord < old_coord && coord <= adj_strike_edge) {
-            let strike_pos = (adj_strike_edge - old_coord) * (orth_coord - old_orth_coord) /
-                             (coord - old_coord) + old_orth_coord;
-            if strike_pos >= edge_start && strike_pos <= edge_end {
+    fn strike(am: &AxisMotion, orth_am: &AxisMotion, edge: &Edge) -> Option<f64> {
+        let adj_strike_edge = if am.vel > 0.0 {edge.pos - BALL_RADIUS}
+                              else {edge.pos + BALL_RADIUS};
+        if (am.vel > 0.0 && am.pos >= adj_strike_edge) ||
+           (am.vel < 0.0 && am.pos <= adj_strike_edge) {
+            let strike_pos = (adj_strike_edge - am.old_pos) * (orth_am.pos - orth_am.old_pos) /
+                             (am.pos - am.old_pos) + orth_am.old_pos;
+            if strike_pos >= edge.start && strike_pos <= edge.end {
                 return Some(strike_pos);
             }
         }
         return None;
     }
 
-    fn normal_rebound(&mut self, from_x: f64, from_y: f64, to_x: f64, to_y: f64) -> bool {
-        if from_x == to_x {
-            // vertical surface
-            match Ball::strike(self.x, self.y, self.old_x, self.old_y, from_x, from_y, to_y) {
-                Some(_) => {
-                    self.x_vel = - self.x_vel;
-                    self.x = 2.0 * from_x - self.x + (self.x_vel.signum() * 2.0 * BALL_RADIUS);
-                    true
-                },
-                _ => {
-                    false
-                },
-            }
-        }
-        else {
-            // horizontal surface
-            match Ball::strike(self.y, self.x, self.old_y, self.old_x, from_y, from_x, to_x) {
-                Some(_) => {
-                    self.y_vel = - self.y_vel;
-                    self.y = 2.0 * from_y - self.y + (self.y_vel.signum() * 2.0 * BALL_RADIUS);
-                    true
-                },
-                _ => {
-                    false
-                },
-            }
+    fn normal_rebound(am: &mut AxisMotion, orth_am: &AxisMotion, edge: &Edge) -> bool {
+        match Ball::strike(am, orth_am, &edge) {
+            Some(_) => {
+                am.vel = - am.vel;
+                am.pos = 2.0 * edge.pos - am.pos + (am.vel.signum() * 2.0 * BALL_RADIUS);
+                true
+            },
+            _ => {
+                false
+            },
         }
     }
 
     fn reposition_overshoot(&mut self, strike_x: f64, strike_y: f64, overshoot: f64) {
-        self.x = strike_x + self.x_vel * overshoot;
-        self.y = strike_y + self.y_vel * overshoot;
+        self.x.pos = strike_x + self.x.vel * overshoot;
+        self.y.pos = strike_y + self.y.vel * overshoot;
     }
 
     fn bat_face_rebound(&mut self, bat_x: f64, speedup: f64) -> bool {
-        match Ball::strike(self.y, self.x, self.old_y, self.old_x, BAT_Y, bat_x, bat_x + BAT_WIDTH) {
+        match Ball::strike(&self.y, &self.x, &Edge::from_width(BAT_Y, bat_x, BAT_WIDTH)) {
             Some(strike_x) => {
                 // these are not the traditional pong bat rebound rules...
-                let face_pos = if self.x_vel > 0.0 {strike_x - bat_x}
+                let face_pos = if self.x.vel > 0.0 {strike_x - bat_x}
                                else {bat_x + BAT_WIDTH - strike_x};
-                let overshoot = (self.y - BAT_Y + BALL_RADIUS) / self.y_vel;
+                let overshoot = (self.y.pos - BAT_Y + BALL_RADIUS) / self.y.vel;
                 if face_pos < 0.3 * BAT_WIDTH {
-                    self.x_vel = self.x_vel.signum() * X_VEL_STEEP * speedup;
+                    self.x.vel = self.x.vel.signum() * X_VEL_STEEP * speedup;
                 }
                 else if face_pos > 0.7 * BAT_WIDTH {
-                    self.x_vel = self.x_vel.signum() * X_VEL_SHALLOW * speedup;
+                    self.x.vel = self.x.vel.signum() * X_VEL_SHALLOW * speedup;
                 }
                 else {
-                    self.x_vel = self.x_vel.signum() * X_VEL_NORMAL * speedup;
+                    self.x.vel = self.x.vel.signum() * X_VEL_NORMAL * speedup;
                 }
-                self.y_vel = - Y_VEL * speedup;
+                self.y.vel = - Y_VEL * speedup;
                 self.reposition_overshoot(strike_x, BAT_Y - BALL_RADIUS, overshoot);
                 true
             },
@@ -173,19 +174,18 @@ impl Ball {
     }
 
     fn corner_strike(&self, cx: f64) -> Option<(f64, f64)> {
-        if self.y > BAT_Y - BALL_RADIUS && self.old_y < BAT_Y {
-            let dx = self.x - self.old_x;
-            let dy = self.y - self.old_y;
+        if self.y.pos > BAT_Y - BALL_RADIUS && self.y.old_pos < BAT_Y {
+            let dx = self.x.pos - self.x.old_pos;
+            let dy = self.y.pos - self.y.old_pos;
             let dr_sq = square(dx) + square(dy);
-            let d = (self.old_x - cx) * (self.y - BAT_Y) - 
-                (self.x - cx) * (self.old_y - BAT_Y);
+            let d = (self.x.old_pos - cx) * (self.y.pos - BAT_Y) - 
+                (self.x.pos - cx) * (self.y.old_pos - BAT_Y);
             let disc = BALL_RADIUS_SQ * dr_sq - square(d);
             if disc > 0.0 {
                 let strike_y = BAT_Y + (-d * dx - dy.abs() * disc.sqrt()) / dr_sq;
-                let strike_x = self.x - (self.y - strike_y) * (self.x - self.old_x) /
-                    (self.y - self.old_y);
-                if self.y >= strike_y && self.old_y < strike_y {
-                    // println!("Corner strike {} {}", strike_x, strike_y);
+                let strike_x = self.x.pos - (self.y.pos - strike_y) * (self.x.pos - self.x.old_pos) /
+                    (self.y.pos - self.y.old_pos);
+                if self.y.pos >= strike_y && self.y.old_pos < strike_y {
                     return Some((strike_x, strike_y));
                 }
             }
@@ -196,16 +196,17 @@ impl Ball {
     fn bat_corner_rebound(&mut self, bat_x: f64, speedup: f64) -> bool {
         // left bat corner
         if let Some((cx, cy)) = self.corner_strike(bat_x) {
-            let overshoot = (self.y - cy) / self.y_vel;
-            self.y_vel = -Y_VEL * speedup;
-            self.x_vel = -X_VEL_SHALLOW * speedup;
+            let overshoot = (self.y.pos - cy) / self.y.vel;
+            self.y.vel = -Y_VEL * speedup;
+            self.x.vel = -X_VEL_SHALLOW * speedup;
             self.reposition_overshoot(cx, cy, overshoot);
             return true;
         }
+        // right bat corner
         else if let Some((cx, cy)) = self.corner_strike(bat_x + BAT_WIDTH) {
-            let overshoot = (self.y - cy) / self.y_vel;
-            self.y_vel = -Y_VEL * speedup;
-            self.x_vel = X_VEL_SHALLOW * speedup;
+            let overshoot = (self.y.pos - cy) / self.y.vel;
+            self.y.vel = -Y_VEL * speedup;
+            self.x.vel = X_VEL_SHALLOW * speedup;
             self.reposition_overshoot(cx, cy, overshoot);
             return true;
         }
@@ -222,36 +223,30 @@ impl Ball {
 
     fn update(&mut self, dt: f64, bat_x: f64, score: &mut Score, speedup: f64) {
         if self.in_play {
-            self.old_x = self.x;
-            self.old_y = self.y;
-            self.x += &self.x_vel * dt;
-            self.y += &self.y_vel * dt;
+            self.x.old_pos = self.x.pos;
+            self.y.old_pos = self.y.pos;
+            self.x.pos += &self.x.vel * dt;
+            self.y.pos += &self.y.vel * dt;
 
-            if self.x_vel > 0.0 &&
-                self.normal_rebound(COURT_WIDTH as f64, -100.0,
-                    COURT_WIDTH as f64, COURT_HEIGHT as f64 + 100.0) {
+            if (self.x.vel > 0.0 &&
+              Ball::normal_rebound(&mut self.x, &self.y, &Edge::infinite(COURT_WIDTH))) ||
+              (self.x.vel < 0.0 && 
+              Ball::normal_rebound(&mut self.x, &self.y, &Edge::infinite(0.0))) {
                 Ball::table_sound();
             }
-            else if self.x_vel < 0.0 &&
-              self.normal_rebound(0.0, -100.0, 0.0, COURT_HEIGHT as f64 + 100.0) {
+            if self.y.vel < 0.0 &&
+              Ball::normal_rebound(&mut self.y, &self.x, &Edge::infinite(0.0)) {
                 Ball::table_sound();
             }
-            if self.y_vel < 0.0 &&
-              self.normal_rebound(-100.0, 0.0, COURT_WIDTH as f64 + 100.0, 0.0) {
-                Ball::table_sound();
-            }
-            else if self.y_vel > 0.0 && self.old_y < BAT_Y - BALL_RADIUS &&
+            else if self.y.vel > 0.0 && self.y.old_pos < BAT_Y - BALL_RADIUS &&
                 self.bat_face_rebound(bat_x, speedup) {
                 Ball::bat_sound();
                 score.increment();
             }
-            if self.y >= BAT_Y && self.x_vel > 0.0 &&
-                self.normal_rebound(bat_x, BAT_Y, bat_x, BAT_Y + BAT_THICKNESS) {
-                Ball::bat_sound();
-            }
-            else if self.y >= BAT_Y && self.x_vel < 0.0 &&
-                self.normal_rebound(bat_x + BAT_WIDTH, BAT_Y, bat_x + BAT_WIDTH,
-                                    BAT_Y + BAT_THICKNESS) {
+            if (self.y.pos >= BAT_Y && self.x.vel > 0.0 &&
+              Ball::normal_rebound(&mut self.x, &self.y, &Edge::from_width(bat_x, BAT_Y, BAT_THICKNESS))) ||
+              (self.y.pos >= BAT_Y && self.x.vel < 0.0 &&
+              Ball::normal_rebound(&mut self.x, &self.y, &Edge::from_width(bat_x + BAT_WIDTH, BAT_Y, BAT_THICKNESS))) {
                 Ball::bat_sound();
             }
             else if self.bat_corner_rebound(bat_x, speedup) {
@@ -263,26 +258,26 @@ impl Ball {
             // What might be more satsfying is to consider the journey between and
             // allow a save via a corner hit or a sideways "push" where the ball has
             // gone too far.
-            if self.y > COURT_HEIGHT as f64 + BALL_RADIUS {
+            if self.y.pos > COURT_HEIGHT + BALL_RADIUS {
                 self.in_play = false;
             }
         }
     }
 
     fn serve(&mut self, speedup: f64) {
-        self.x_vel = (if rand::thread_rng().gen() {X_VEL_NORMAL} else {- X_VEL_NORMAL}) * speedup;
-        self.y_vel = Y_VEL * speedup;
-        self.x = rand::thread_rng().gen_range(BALL_RADIUS as u32 + 1, COURT_WIDTH - BALL_RADIUS as u32) as f64;
-        self.y = rand::thread_rng().gen_range(BALL_RADIUS as u32 + 1, COURT_HEIGHT / 3) as f64;
-        self.old_x = self.x - self.x_vel;
-        self.old_y = self.y - self.y_vel;
+        self.x.vel = (if rand::thread_rng().gen() {X_VEL_NORMAL} else {- X_VEL_NORMAL}) * speedup;
+        self.y.vel = Y_VEL * speedup;
+        self.x.pos = rand::thread_rng().gen_range(BALL_RADIUS as u32 + 1, (COURT_WIDTH - BALL_RADIUS) as u32) as f64;
+        self.y.pos = rand::thread_rng().gen_range(BALL_RADIUS as u32 + 1, COURT_HEIGHT as u32 / 3) as f64;
+        self.x.old_pos = self.x.pos - self.x.vel;
+        self.y.old_pos = self.y.pos - self.y.vel;
         self.in_play = true;
     }
 
     fn render(&self, c: Context, g: &mut G2d) {
         if self.in_play {
             let ball_shape = ellipse::circle(0.0, 0.0, BALL_RADIUS);
-            ellipse(self.colour, ball_shape, c.transform.trans(self.x, self.y), g);
+            ellipse(self.colour, ball_shape, c.transform.trans(self.x.pos, self.y.pos), g);
         }
     }
 }
@@ -335,7 +330,11 @@ impl Balls {
     }
 }
 
-fn render_instruction(c: Context, g: &mut G2d, glyphs: &mut Glyphs, x: f64, y: f64, txt: &str) {
+fn square(n: f64) -> f64 {
+    n * n
+}
+
+fn render_text(c: Context, g: &mut G2d, glyphs: &mut Glyphs, x: f64, y: f64, txt: &str) {
     text::Text::new_color([1.0, 1.0, 1.0, 1.0], 32).draw(
         txt,
         glyphs,
@@ -349,7 +348,7 @@ fn main() {
     let opengl = OpenGL::V2_1;
     let mut window: PistonWindow = WindowSettings::new(
         "Pongish",
-        (COURT_WIDTH, COURT_HEIGHT)
+        (COURT_WIDTH as u32, COURT_HEIGHT as u32)
     )
     .exit_on_esc(true)
     .opengl(opengl)
@@ -379,7 +378,7 @@ fn main() {
     let factory = window.factory.clone();
     let mut glyphs = Glyphs::new(font, factory, TextureSettings::new()).unwrap();
 
-    let mut bat_x = (COURT_WIDTH as f64 + BAT_WIDTH) / 2.0;
+    let mut bat_x = (COURT_WIDTH + BAT_WIDTH) / 2.0;
     let mut score = Score::new();
     let mut game_mode = GameMode::StartUp;
 
@@ -419,14 +418,10 @@ fn main() {
                                     ).unwrap();
                             }
 
-                            render_instruction(c, g, &mut glyphs, 200.0, 400.0,
-                                               "Pongish - somewhat Pong-like");
-                            render_instruction(c, g, &mut glyphs, 200.0, 475.0,
-                                               "Press space to play");
-                            render_instruction(c, g, &mut glyphs, 200.0, 550.0,
-                                               "Use the mouse to control the bat");
-                            render_instruction(c, g, &mut glyphs, 200.0, 625.0,
-                                               "Press escape to exit at any time");
+                            render_text(c, g, &mut glyphs, 200.0, 400.0, "Pongish - somewhat Pong-like");
+                            render_text(c, g, &mut glyphs, 200.0, 475.0, "Press space to play");
+                            render_text(c, g, &mut glyphs, 200.0, 550.0, "Use the mouse to control the bat");
+                            render_text(c, g, &mut glyphs, 200.0, 625.0, "Press escape to exit at any time");
                         }
                     });
                     if let Some(Button::Keyboard(Key::Space)) = e.release_args() {
@@ -452,8 +447,8 @@ fn main() {
                         if bat_x < - BAT_WIDTH {
                             bat_x = - BAT_WIDTH;
                         }
-                        if bat_x > COURT_WIDTH as f64 {
-                            bat_x = COURT_WIDTH as f64;
+                        if bat_x > COURT_WIDTH {
+                            bat_x = COURT_WIDTH;
                         }
                     });
 
@@ -477,12 +472,9 @@ fn main() {
                         window.draw_2d(&e, |c, g| {
                             clear([0.0, 0.0, 0.0, 1.0], g);
                             score.render(c, g, &mut glyphs);
-                            render_instruction(c, g, &mut glyphs, 200.0, 400.0,
-                                               "Game Over");
-                            render_instruction(c, g, &mut glyphs, 200.0, 475.0,
-                                               "Press space to play again");
-                            render_instruction(c, g, &mut glyphs, 200.0, 550.0,
-                                               "Press escape to exit at any time");
+                            render_text(c, g, &mut glyphs, 200.0, 400.0, "Game Over");
+                            render_text(c, g, &mut glyphs, 200.0, 475.0, "Press space to play again");
+                            render_text(c, g, &mut glyphs, 200.0, 550.0, "Press escape to exit at any time");
                         });
                     }
                     if let Some(Button::Keyboard(Key::Space)) = e.release_args() {
